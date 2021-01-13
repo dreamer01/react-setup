ec7057cbdaadeeb2ddc43ea07975b4d250ad6438
Pushpendra Singh <psnitsri01@gmail.com>
2021-01-13 22:06:18 +0530

    Adding HTMLWebpackPlugin for auto appending bundled files

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
  
 module.exports = {
   entry: path.resolve(__dirname, './src/index.js'),
@@ -18,6 +19,9 @@ module.exports = {
     path: path.resolve(__dirname, './dist'),
     filename: 'bundle.js',
   },
+  plugins: [new HtmlWebpackPlugin({
+    template: path.resolve(__dirname, './public/index.html')
+  })],
   devServer: {
     contentBase: path.resolve(__dirname, './dist'),
   },
