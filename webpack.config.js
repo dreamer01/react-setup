f2fb4a41fe312450064cf7662e3e1dae27bd5fc8
Pushpendra Singh <psnitsri01@gmail.com>
2021-01-13 20:11:55 +0530

    Initial babel setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,20 @@
 const path = require('path');
  
 module.exports = {
+  mode: 'development',
   entry: path.resolve(__dirname, './src/index.js'),
+  module: {
+    rules: [
+      {
+        test: /\.(js)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
+  },
+  resolve: {
+    extensions: ['*', '.js']
+  },
   output: {
     path: path.resolve(__dirname, './dist'),
     filename: 'bundle.js',
