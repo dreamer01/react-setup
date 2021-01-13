358310c13dd6f1874695619dddce85859db50cb7
Pushpendra Singh <psnitsri01@gmail.com>
2021-01-13 21:04:15 +0530

    Adding react to project

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-const welcome = () => console.log("Welcome to console, js playground.")
-
-welcome();
+import React from 'react';
+import ReactDOM from 'react-dom';
+ 
+const title = 'React with Webpack and Babel';
+ 
+ReactDOM.render(
+  <div>{title}</div>,
+  document.getElementById('root')
+);
